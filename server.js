4e7b29fe944859160a10e57733a244ec2b0dd05d
Leire Litwin <leirelitwin@sfs-wifi-dhcp-10-143-208-126.wifi.sfsu.edu>
2018-04-19 18:29:29 -0700

    port changed

--- server.js
+++ server.js
@@ -44,6 +44,7 @@ app.get('/', function(req, res){
 */
 
 // listen for requests
-app.listen(3000, function(){
+
+app.listen(process.env.PORT || 3000, function(){
     console.log("Server is listening on port 3000");
 });
