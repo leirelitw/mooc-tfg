9761bb31173cfd52462230d452ae546c94188d98
Leire Litwin <leirelitwin@sfs-wifi-dhcp-10-143-208-126.wifi.sfsu.edu>
2018-04-19 17:57:17 -0700

    make it better

--- config/database.config.js
+++ config/database.config.js
@@ -1,3 +1,6 @@
 module.exports = {
-    url: 'mongodb://localhost:27017/mooc'
+    //url: 'mongodb://localhost:27017/mooc'
+    //url: 'mongodb://<dbuser>:<dbpassword>@ds159866.mlab.com:59866/heroku_8356kf3j'
+    url: 'mongodb://leirelitwin:74734164B@ds159866.mlab.com:59866/heroku_8356kf3j'
+
 }
