9dc234ea7db33954b00862bb6e21919b7c7e3342
Leire Litwin <leirelitwin@MacBook-Pro-de-Leire.local>
2018-05-15 21:58:48 -0700

    update linear graphs
    linear graphs display data from database

--- app/routes/graphs.routes.js
+++ app/routes/graphs.routes.js
@@ -1,11 +1,19 @@
-module.exports = router;
-
 var express = require('express');
 var router = express.Router();
+var universities = require('../controllers/university.controller.js');
 
 /* GET home page. */
+
 router.get('/', function(req, res, next) {
-  res.render('graphs', { title: 'MOOCs | Leire Litwin' });
+  universities.findAll().then( function(dataRetrieved){
+    //console.log(dataRetrieved);
+    res.render('graphs', { title: 'MOOCs Ranking | Leire Litwin',
+    data: dataRetrieved
+    });
+  }, function (reason) {
+    console.log("argh"+reason);
+  });
 });
+
 
 module.exports = router;
